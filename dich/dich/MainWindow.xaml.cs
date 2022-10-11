d239ecfe2de42f9b80ef25742465f078c242f07b
Murzachilo <88836101+Murzachilo@users.noreply.github.com>
2022-10-12 00:17:12 +0300

    Add files via upload

---
+++ dich/dich/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dich
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn;
+        public MainWindow()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=" + System.Environment.MachineName + ";Initial Catalog=CompStrahOfAuto;Integrated Security=True";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox1.Text == "" || textBox2.Password == "")
+            {
+                MessageBox.Show("логин или пароль пуст", textBox1.Text);
+                return;
+            }  // Проверка на заполнение
+            else
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                DataTable useresTable = new DataTable();
+                SqlCommand comm = new SqlCommand("SELECT * FROM Сотрудники WHERE Login = '" + textBox1.Text + "' AND Password = '" + textBox2.Password + "' AND IdRole = 0", conn);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(comm);
+                comm.ExecuteNonQuery();
+                adapter.Fill(useresTable);
+                if (useresTable.Rows.Count > 0)
+                {
+                    this.Hide();
+                    adminMenu dlg = new adminMenu();
+                    dlg.Show();
+                    //Вход как админ
+
+                    // Проверка на админа
+                }
+                else
+                {
+                    SqlCommand com = new SqlCommand("SELECT * FROM Сотрудники WHERE Login = '" + textBox1.Text + "' AND Password = '" + textBox2.Password + "'", conn);
+                    DataTable useresTabl = new DataTable();
+
+                    SqlDataAdapter adapte = new SqlDataAdapter(com);
+                    com.ExecuteNonQuery();
+                    adapte.Fill(useresTabl);
+                    if (useresTabl.Rows.Count > 0)
+                    {
+                        this.Hide();
+                        Menu dlg = new Menu();
+                        dlg.Show();
+                        // Вход как пользователя
+                    }
+                    else
+                    {
+                        MessageBox.Show("Неверный логин или пароль");
+                        // Уведомление о не входе
+                    }
+
+                }
+                // Проверка на пользователя
+            }
+        }
+    }
+}
---
+++ dich/dich/TablePrekoldes.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dich
+{
+    /// <summary>
+    /// Логика взаимодействия для TablePrekoldes.xaml
+    /// </summary>
+    public partial class TablePrekoldes : Window
+    {
+        SqlConnection conn;
+        SqlDataAdapter adapter;
+        DataTable phonesTable;
+        string AccId;
+        bool s = false;
+        int typeSql;
+        public TablePrekoldes()
+        {
+            InitializeComponent();
+            conn = new SqlConnection();
+            //сonn = new SqlConnection();
+            //adapter = new SqlDataAdapter(command);
+            conn.ConnectionString = "Data Source=localhost;Initial Catalog=CompStrahOfAuto;Integrated Security=True";
+            phonesGrid.CanUserAddRows = false;
+        }
+
+        private void BtnForm_Click(object sender, RoutedEventArgs e)
+        {
+            /*TaskWindow taskWindow = new TaskWindow();            // Изменение 
+            DataRowView row = (DataRowView)phonesGrid.SelectedItems[0];
+            taskWindow.rw = row;
+            taskWindow.Show();
+            //MessageBox.Show(taskWindow.strQuery);
+            this.Close();
+            /*if (conn.State == ConnectionState.Closed) conn.Open();
+            DataTable phonesTable = new DataTable("Phones");
+
+            SqlCommand comm = new SqlCommand(taskWindow.strQuery, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            comm.ExecuteNonQuery();
+            comm.CommandText = "SELECT * FROM Аккаунты";
+            comm.ExecuteNonQuery();
+            adapter.Fill(phonesTable);
+            phonesGrid.ItemsSource = phonesTable.DefaultView;*/
+        }
+
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            DataTable phonesTable = new DataTable();
+            SqlCommand comm = new SqlCommand("SELECT Серия, Номер, [Дата рождения], [Дата начала стажа] FROM [Водительское удостверение]", conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            comm.ExecuteNonQuery();
+            adapter.Fill(phonesTable);
+            phonesGrid.ItemsSource = phonesTable.DefaultView;
+            //phonesGrid.SelectedIndex = 0;
+            phonesGrid.Focus();
+            phonesGrid.SelectedItem = phonesGrid.Items[0];
+            phonesGrid.ScrollIntoView(phonesGrid.Items[0]);
+        }
+
+        private void BtnInsert_Click(object sender, RoutedEventArgs e)
+        {
+
+            //TaskWindow taskWindow = new TaskWindow();
+            //DataRowView row = (DataRowView)phonesGrid.SelectedItems[0];      //Обработчик добавить
+            //taskWindow.rw = row;
+            //taskWindow.rw = null;
+            //taskWindow.Show();
+            //this.Close();
+            /*MessageBox.Show(taskWindow.strQuery);
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            DataTable phonesTable = new DataTable("Аккаунт");
+
+            SqlCommand comm = new SqlCommand(taskWindow.strQuery, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(comm);
+            comm.ExecuteNonQuery();
+            comm.CommandText = "SELECT * FROM Аккаунт";
+            comm.ExecuteNonQuery();
+            adapter.Fill(phonesTable);
+            phonesGrid.ItemsSource = phonesTable.DefaultView;*/
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            int count_row = phonesGrid.Items.Count;
+            MessageBox.Show(Convert.ToString(count_row));
+            try
+            {
+
+                DataRowView row = (DataRowView)phonesGrid.SelectedItems[0];
+
+                // MessageBox.Show(Convert.ToString(row["Title"]));
+                AccId = Convert.ToString(row["Код"]);
+                MessageBox.Show("DELETE  FROM Аккаунт WHERE Id = '" + AccId + "'");
+
+
+                if (conn.State == ConnectionState.Closed) conn.Open();
+                DataTable phonesTable = new DataTable("Аккаунт");
+                SqlCommand comm = new SqlCommand("DELETE  FROM Аккаунт WHERE Код = '" + AccId + "'", conn);
+                SqlDataAdapter adapter = new SqlDataAdapter(comm);
+                comm.ExecuteNonQuery();
+                comm.CommandText = "SELECT * FROM Аккаунт";
+                comm.ExecuteNonQuery();
+                adapter.Fill(phonesTable);
+                phonesGrid.ItemsSource = phonesTable.DefaultView;
+
+            }
+            catch
+            {
+                MessageBox.Show("нет данных");
+                return;
+            }
+
+
+
+        }
+
+
+        private void phonesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
